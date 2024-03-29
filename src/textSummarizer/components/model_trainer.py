b35ea9147a4277ef21e7dd71f862c3abf6a9a0a7
Ajith <ajithkannan13698@gmail.com>
2024-03-29 21:48:34 +0530

    retained trainer.py

--- src/textSummarizer/components/model_trainer.py
+++ src/textSummarizer/components/model_trainer.py
@@ -5,53 +5,48 @@ from datasets import load_dataset, load_from_disk
 from textSummarizer.entity import ModelTrainerConfig
 import torch
 import os
-from accelerate import Accelerator, DataLoaderConfiguration
 
-import os
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:1024"
 
 class ModelTrainer:
     def __init__(self, config: ModelTrainerConfig):
         self.config = config
 
+
+    
     def train(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
         tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
         model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
         seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
         
-        # Loading data 
+        #loading data 
         dataset_samsum_pt = load_from_disk(self.config.data_path)
 
-        # Define DataLoaderConfiguration
-        dataloader_config = DataLoaderConfiguration(dispatch_batches=None, split_batches=False, even_batches=True, use_seedable_sampler=True)
+        # trainer_args = TrainingArguments(
+        #     output_dir=self.config.root_dir, num_train_epochs=self.config.num_train_epochs, warmup_steps=self.config.warmup_steps,
+        #     per_device_train_batch_size=self.config.per_device_train_batch_size, per_device_eval_batch_size=self.config.per_device_train_batch_size,
+        #     weight_decay=self.config.weight_decay, logging_steps=self.config.logging_steps,
+        #     evaluation_strategy=self.config.evaluation_strategy, eval_steps=self.config.eval_steps, save_steps=1e6,
+        #     gradient_accumulation_steps=self.config.gradient_accumulation_steps
+        # ) 
 
-        # Instantiate Accelerator with DataLoaderConfiguration
-        accelerator = Accelerator(dataloader_config=dataloader_config)
 
         trainer_args = TrainingArguments(
             output_dir=self.config.root_dir, num_train_epochs=1, warmup_steps=500,
             per_device_train_batch_size=1, per_device_eval_batch_size=1,
             weight_decay=0.01, logging_steps=10,
             evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
-            gradient_accumulation_steps=2
+            gradient_accumulation_steps=16
         ) 
 
         trainer = Trainer(model=model_pegasus, args=trainer_args,
-                          tokenizer=tokenizer, data_collator=seq2seq_data_collator,
-                          train_dataset=dataset_samsum_pt["test"], 
-                          eval_dataset=dataset_samsum_pt["validation"])
+                  tokenizer=tokenizer, data_collator=seq2seq_data_collator,
+                  train_dataset=dataset_samsum_pt["test"], 
+                  eval_dataset=dataset_samsum_pt["validation"])
         
         trainer.train()
 
-        # Save model
-        model_pegasus.save_pretrained(os.path.join(self.config.root_dir, "pegasus-samsum-model"))
-        # Save tokenizer
-        tokenizer.save_pretrained(os.path.join(self.config.root_dir, "tokenizer"))
-
-# # Create an instance of ModelTrainerConfig
-# config = ModelTrainerConfig()  # You may need to provide necessary configurations
-
-# # Create an instance of ModelTrainer and start training
-# trainer = ModelTrainer(config)
-# trainer.train()
+        ## Save model
+        model_pegasus.save_pretrained(os.path.join(self.config.root_dir,"pegasus-samsum-model"))
+        ## Save tokenizer
+        tokenizer.save_pretrained(os.path.join(self.config.root_dir,"tokenizer"))
